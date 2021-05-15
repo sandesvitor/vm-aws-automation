960d1cb211f8e6b1f03b0d8bdee4e74ef08e5506
sandesvitor <sandesvitor@gmail.com>
2021-05-15 15:53:42 -0300

    Formated main.tf and variables.tf

--- main.tf
+++ main.tf
@@ -3,24 +3,24 @@ terraform {
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 module "network" {
-    source                  = "./modules/terraform-aws-network"
-    main_network_block      = var.main_network_block
-    subnet_prefix_extension = var.subnet_prefix_extension
+  source                  = "./modules/terraform-aws-network"
+  main_network_block      = var.main_network_block
+  subnet_prefix_extension = var.subnet_prefix_extension
 }
 
 module "security_group" {
-    source      = "./modules/terraform-aws-security-groups"
-    ingresses   = var.ingresses
-    egresses    = var.egresses
+  source    = "./modules/terraform-aws-security-groups"
+  ingresses = var.ingresses
+  egresses  = var.egresses
 }
 
 module "ec2_instance" {
-    source          = "./modules/terraform-aws-instance"
-    ami             = var.ami 
-    instance_type   = var.instance_type
-    security_groups = [module.security_group.name]
+  source          = "./modules/terraform-aws-instance"
+  ami             = var.ami
+  instance_type   = var.instance_type
+  security_groups = [module.security_group.name]
 }
